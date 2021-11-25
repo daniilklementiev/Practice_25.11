aca40aab66b63b6a7da883d17e52180d54521759
Klementiev Daniil <Klem_em31@student.itstep.org>
2021-11-25 16:07:51 +0200

    123

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -1,22 +1,29 @@
-// Practice.cpp : Defines the entry point for the application.
+﻿// Practice.cpp : Defines the entry point for the application.
 //
 
 #include "framework.h"
 #include "Practice.h"
 
-#define MAX_LOADSTRING 100
+#define MAX_LOADSTRING          100
+#define CMD_BUTTON_START        1001
+#define THREADCOUNT             3
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HWND total;
+HWND active;
+HANDLE mutex = NULL;
+HANDLE Semaphore;
+
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
+void                StartThread();
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -26,7 +33,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: Place code here.
-
+   
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_PRACTICE, szWindowClass, MAX_LOADSTRING);
@@ -75,7 +82,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PRACTICE));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = CreateSolidBrush(RGB(10,100,80));
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PRACTICE);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -125,12 +132,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+    case WM_CREATE:
+    {
+        CreateWindowW(L"Button", L"Start", WS_CHILD | WS_VISIBLE, 10, 10, 75, 23, hWnd, (HMENU)CMD_BUTTON_START, hInst, 0);
+        total = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE, 10, 90, 250, 300, hWnd, 0, hInst, 0);
+        active = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE, 320, 90, 250, 300, hWnd, 0, hInst, 0);
+        mutex = CreateMutex(NULL, FALSE, NULL);
+        /*if (mutex == NULL) {
+            MessageBoxW(hWnd, L"Mutex error", L"Error", MB_OK);
+            return -1;
+        }
+        else {
+            SendMessage(active, LB_ADDSTRING, 100, (LPARAM)L"Mutex OK");
+        }*/
+
+        Semaphore = CreateSemaphore(NULL, NULL, 3, NULL);
+        if (Semaphore == NULL)
+        {
+            MessageBoxW(0, L"Semaphore error!", L"Error", MB_ICONERROR);
+        }
+        else {
+            SendMessage(active, LB_ADDSTRING, 100, (LPARAM)L"Semaphore OK");
+        }
+    }
+        break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
             // Parse the menu selections:
             switch (wmId)
             {
+            case CMD_BUTTON_START:
+                StartThread();
+                break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
@@ -177,4 +211,74 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+/**********************/
+HANDLE SemaphoreHandle[THREADCOUNT];
+
+struct ThreadData {
+    int month;
+    float percent;
+    ThreadData(int month, float percent) : month{ month }, percent{ percent } {}
+};
+
+int activeThreadsCount;
+
+DWORD WINAPI ThreadProc(LPVOID params) {
+
+    WCHAR txt[100];
+    ThreadData* data = (ThreadData*)params;
+
+    DWORD waitResult = WaitForSingleObject(Semaphore, 10);
+    switch (waitResult)
+    {
+    case WAIT_OBJECT_0: // NORMAL
+        activeThreadsCount++;
+
+        _snwprintf_s(txt, 100, L"thread - %d, month - %d", activeThreadsCount, data->month);
+        SendMessageW(active, LB_ADDSTRING, 100, (LPARAM)txt);
+        _snwprintf_s(txt, 100, L"%d %f", data->month, data->percent);
+        SendMessageW(total, LB_ADDSTRING, 100, (LPARAM)txt);
+        
+        activeThreadsCount--;
+        ReleaseSemaphore(Semaphore, 1, NULL);
+    
+        break;
+
+    case WAIT_TIMEOUT:  // TIMEOUT
+        _snwprintf_s(txt, 100, L"%d timeout ", data->month);
+        SendMessageW(total, LB_ADDSTRING, 100, (LPARAM)txt);
+        break;
+
+    default:
+    {}
+    }
+
+
+    return 0;
+}
+
+void StartThread() {
+    WCHAR txt[100];
+    SendMessageW(total, LB_RESETCONTENT, 0, 0);
+    SendMessageW(active, LB_RESETCONTENT, 0, 0);
+    
+   /* for (int i = 1; i <= 12; i++)
+    {
+        CreateThread(NULL, 0, ThreadProc, new ThreadData(i, 10), 0, NULL);
+    }*/
+
+    for (int i = 0; i < THREADCOUNT; i++)
+    {
+        SemaphoreHandle[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadProc, new ThreadData(i, 10), 0, 0);
+        if (SemaphoreHandle[i] == NULL)
+        {
+            _snwprintf_s(txt, 100, L"CreateThread error: %d", GetLastError());
+            SendMessage(active, LB_ADDSTRING, 100, (LPARAM)txt);
+            return;
+        }
+
+    }
+
+   
 }
